de06db35560abd8324607d397d8641a56e9257bd
Graham Fujisawa <sfrrki@gmail.com>
2023-03-06 20:11:46 -0800

    add ex4, compound where clauses

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -10,3 +10,16 @@ puts "----------"
 Surrey = Store.create(name: "Surrey", annual_revenue: 224000, womens_apparel: true, mens_apparel: false)
 Whistler = Store.create(name: "Whistler", annual_revenue: 1900000, womens_apparel: false, mens_apparel: true)
 Yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, womens_apparel: true, mens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |store|
+  puts store.name, store.annual_revenue
+end
+
+@womens_stores = Store.where(womens_apparel: true, annual_revenue: ...1000000)
+# @wom_rev = Store.where("id < ?", 1000000)
+
+@womens_stores.each do |store|
+  puts store.name
+end
